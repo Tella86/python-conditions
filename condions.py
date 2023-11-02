41fa347bd9a6a1fc8a92adbdace68275a8b2f541
ZIROH KATANA MAE <tellazone0@gmail.com>
2023-11-02 20:18:15 +0300

    Add files via upload

---
+++ condions.py
@@ -0,0 +1,40 @@
+#if condition
+# if ......else statement
+# if ... elif..else
+
+relationship = True
+if relationship: #condition
+    #body
+    print("Taken")
+# if else statement
+   
+loyal_relationship = True
+
+if loyal_relationship:
+    #body
+    print("Give your girlfriend your phone")
+else: # condition
+#body
+    print("you walk in her heels and mum jeans to work")
+
+num = 2
+if num >=20:
+    print("print number Great or less than")
+#  if ...elif...else
+fuliza  = True
+okoa = False
+if fuliza:
+        print("usiweke kwa hii number")
+elif okoa:
+        print("Reverse call")
+else:
+        print("Maisha London, soft life")
+marks = 64
+if marks >=98 and marks <=100:#99-100
+      print("A")
+elif marks >=80:
+      print("B")
+elif marks >=64:
+      print("B-")
+else:
+      print("Fail")
